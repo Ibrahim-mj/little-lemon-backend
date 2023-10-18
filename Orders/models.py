72cdbc239fe905c7cc59e0657a8643751c962cdc
Ibrahim Abdulmojeed <ibraheemabdulmojeed1@gmail.com>
2023-10-18 23:52:28 +0100

    Created Cart and related model

--- Orders/models.py
+++ Orders/models.py
@@ -1,24 +1,11 @@
 from django.db import models
-
-class Category(models.Model):
-    slug = models.SlugField(unique=True)
-    title =  models.CharField(max_length=255)
-
-    def __str__(self) -> str:
-        return self.title
-
-class MenuItem(models.Model):
-    title = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    inventory = models.SmallIntegerField()
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
-
-    def __str__(self) -> str:
-        return self.title
-
+from menu_items.models import MenuItem
 
 class Cart(models.Model):
-    items = models.ManyToManyField(MenuItem, through='OrderItem', related_name='order_item')
+    """
+    A model representing a shopping cart for a user.
+    """
+    items = models.ManyToManyField(MenuItem, through='CartItem', related_name='order_item')
     owner = models.ForeignKey('auth.User', related_name='cart', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -29,22 +16,48 @@ class Cart(models.Model):
 
     def calculate_total_price(self):
         """
-        Calculate and update the order total price
+        Calculate and update the order total price based on the items in the cart.
         """
         self.total_price = sum([item.price for item in self.items.all()])
         self.save()
 
     def calculate_quantity(self):
         """
-        Calculate and update the order quantity
+        Calculate and update the order quantity based on the items in the cart.
         """
         self.quantity = self.items.all().count()
         self.save()
 
-class OrderItem(models.Model):
-    order_list = models.ForeignKey(Cart, on_delete=models.CASCADE)
+class CartItem(models.Model):
+    """
+    A model representing an item in an order.
+
+    Attributes:
+        cart (Cart): The cart that the order item belongs to.
+        menu_item (MenuItem): The menu item that was ordered.
+        quantity (int): The quantity of the menu item that was ordered.
+    """
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return f"{self.quantity} x {self.menu_item.title} for {self.order_list.owner.username}"
+
+# I am still not clear about the order concepts yet
+
+# class Order(models.Model):
+#     """
+#     A model representing an order.
+
+#     Attributes:
+#         order_list (Cart): The cart that the order item belongs to.
+#         menu_item (MenuItem): The menu item that was ordered.
+#         quantity (int): The quantity of the menu item that was ordered.
+#     """
+#     order_list = models.ForeignKey(Cart, on_delete=models.CASCADE)
+#     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+#     quantity = models.PositiveIntegerField(default=1)
+
+#     def __str__(self):
+#         return f"{self.quantity} x {self.menu_item.title} for {self.order_list.owner.username}"
